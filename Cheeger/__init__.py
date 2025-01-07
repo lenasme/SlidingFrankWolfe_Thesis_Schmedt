e02421ee612f3e2a78b68ae1ac231a8dd4f785a0
lenasme <132449066+lenasme@users.noreply.github.com>
2025-01-07 21:21:46 +0100

    Update __init__.py

--- Cheeger/__init__.py
+++ Cheeger/__init__.py
@@ -1,1 +1,5 @@
-
+from .compute_cheeger import compute_cheeger
+from .simple_set import *
+from .integrable_function import IntegrableFunction
+from .tools import winding, resample
+from .plot_utils import *
