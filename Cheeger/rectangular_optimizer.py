ea56ac6d959f18de9b16050ab73aa0664344ea70
lenasme <132449066+lenasme@users.noreply.github.com>
2025-01-09 19:32:35 +0100

    Update rectangular_optimizer.py

--- Cheeger/rectangular_optimizer.py
+++ Cheeger/rectangular_optimizer.py
@@ -4,10 +4,10 @@ from .simple_set import SimpleSet
 
 def objective(boundary_vertices, eta):
   rect_set = SimpleSet(boundary_vertices)
-  x_min = boundary_vertices[0][0]
-  x_max = boundary_vertices[2][0]
-  y_min = boundary_vertices[0][1]
-  y_max = boundary_vertices[2][1]
+  x_min = boundary_vertices[0]
+  x_max = boundary_vertices[4]
+  y_min = boundary_vertices[1]
+  y_max = boundary_vertices[5]
   if x_max <= x_min or y_max <= y_min:
     return np.inf
   res = rect_set.compute_perimeter() /  np.abs(rect_set.compute_weighted_area(eta) )
