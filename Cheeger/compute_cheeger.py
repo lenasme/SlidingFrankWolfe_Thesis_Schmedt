0788c22894684d119981522d1162ae85345f5b94
lenasme <132449066+lenasme@users.noreply.github.com>
2025-01-10 09:53:51 +0100

    Update compute_cheeger.py

--- Cheeger/compute_cheeger.py
+++ Cheeger/compute_cheeger.py
@@ -127,10 +127,14 @@ def compute_cheeger(eta, grid_size_fm, max_iter_fm=10000, convergence_tol_fm=Non
     print("x_min", x_min, "x_max", x_max, "y_min", y_min, "y_max", y_max )
 
     outer_vertices= np.array([x_min, x_max, y_min, y_max])
+    rectangle_boundary_vertices= np.array([[x_min,y_min], [x_min, y_max], [x_max, y_max], [x_max, y_min]])
+    rectangle_set = SimpleSet(rectangle_boundary_vertices)
 
+    print(rectangle_set.)
+                      
     result = minimize(objective, outer_vertices, args=(eta,), bounds=[(0,1),(0,1), (0,1), (0,1)])
                       
-    #rectangle_set = SimpleSet(rectangle_boundary_vertices)
+    
 
     optimal_rectangle = result.x
     optimal_objective = result.fun
