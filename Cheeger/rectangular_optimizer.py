757246f73fd4bb15e68e3e37d27abed304a82c98
lenasme <132449066+lenasme@users.noreply.github.com>
2025-01-10 09:27:53 +0100

    Update rectangular_optimizer.py

--- Cheeger/rectangular_optimizer.py
+++ Cheeger/rectangular_optimizer.py
@@ -8,8 +8,10 @@ def objective(boundary_vertices, eta):
   y_min = boundary_vertices[2]
   y_max = boundary_vertices[3]
   if x_max <= x_min or y_max <= y_min:
-    print("die reihenfolge der vertices passt nicht!")
-    return np.inf
+    #print("die reihenfolge der vertices passt nicht!")
+    #return np.inf
+    penalty = 1e6 + (x_min - x_max) ** 2 + (y_min - y_max) ** 2
+    return penalty
   rectangle_boundary_vertices= np.array([[x_min, y_min], [x_min, y_max], [x_max, y_max], [x_max, y_min]])
   rect_set = SimpleSet(rectangle_boundary_vertices)
   res = rect_set.compute_perimeter() /  np.abs(rect_set.compute_weighted_area(eta) )
