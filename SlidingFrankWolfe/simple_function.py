9b453b0706d5f1aa583bf4453d0e6ff6b1c69407
lenasme <lena.schmedt@gmx.de>
2025-01-15 12:06:59 +0100

    nochmal weights function atome in SimpleFunction geändert

--- SlidingFrankWolfe/simple_function.py
+++ SlidingFrankWolfe/simple_function.py
@@ -91,7 +91,8 @@ class SimpleFunction:
         if version == 0:
             comined_image = np.zeros((grid_size, grid_size))
             for atom in self.atoms:
-                atom_image = atom.transform_into_image(grid_size)
+                atom_simple_function = SimpleFunction(atom)
+                atom_image = atom_simple_function.transform_into_image(grid_size)
                 combined_image += atom.weight * atom_image
             truncated_transform = Trunc_Fourier(combined_image, cut_f)
             return np.real(truncated_transform)
@@ -99,7 +100,8 @@ class SimpleFunction:
         elif version == 1:
             observation = []
             for atom in self.atoms:
-                atom_image = atom.transform_into_image(grid_size)
+                atom_simple_function = SimpleFunction(atom)
+                atom_image = atom_simple_function.transform_into_image(grid_size)
                 truncated_transform = Trunc_Fourier(atom.weight * atom_image, cut_f)
                 observations.append(np.real(truncated_transform))
             return np.array(observations)
