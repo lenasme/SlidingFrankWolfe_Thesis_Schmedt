9e84c3b951939305b2feb1eb6c0774d1100baae5
lenasme <lena.schmedt@gmx.de>
2025-01-16 19:46:38 +0100

    signum mit rein für linera_fit_weights

--- SlidingFrankWolfe/simple_function.py
+++ SlidingFrankWolfe/simple_function.py
@@ -167,7 +167,7 @@ class SimpleFunction:
         scaled_atoms = [
             ZeroWeightedIndicatorFunction(atom.support, atom.weight * (1- gamma))
             for atom in self.atoms[:-1]]
-        new_atom = ZeroWeightedIndicatorFunction(self.atoms[-1].support, self.atoms[-1].weight * (gamma * M/ self.atoms[-1].support.compute_perimeter_rec() ))
+        new_atom = ZeroWeightedIndicatorFunction(self.atoms[-1].support, self.atoms[-1].weight * (gamma * M * np.sign(self.atoms[-1].support.compute_weighted_area_rec())/ self.atoms[-1].support.compute_perimeter_rec() ))
         return SimpleFunction([scaled_atoms, new_atom])
 
 
