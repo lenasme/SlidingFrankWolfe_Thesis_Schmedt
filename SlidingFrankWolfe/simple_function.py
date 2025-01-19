f200b80fd3d3f0f82a3a871acce910ca4bddd6ef
lenasme <lena.schmedt@gmx.de>
2025-01-19 18:12:24 +0100

    in linear fir weights

--- SlidingFrankWolfe/simple_function.py
+++ SlidingFrankWolfe/simple_function.py
@@ -174,8 +174,11 @@ class SimpleFunction:
             ZeroWeightedIndicatorFunction(atom.support, atom.weight * (1- gamma))
             for atom in self.atoms[:-1]]
         new_weight = - self.atoms[-1].weight * (gamma * M * np.sign(self.atoms[-1].support.compute_weighted_area_rec(f))/ self.atoms[-1].support.compute_perimeter_rec())
+        print("self.atoms perimeter... größer als 4???", self.atoms[-1].support.compute_perimeter_rec())
+        print("self.atoms.rect_area in weight berechnung:", self.atoms[-1].support.compute_weighted_area_rec(f))
         new_atom = ZeroWeightedIndicatorFunction(self.atoms[-1].support, new_weight)
         print(new_weight)
+        
         scaled_atoms.append(new_atom)
         self.atoms = scaled_atoms
         print("die atome der angepasstenfunction:", self.atoms)
