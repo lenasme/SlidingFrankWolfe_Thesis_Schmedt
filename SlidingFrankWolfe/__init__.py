5d9cd4711f5158b8d1da56fe05a90ef098f16649
lenasme <lena.schmedt@gmx.de>
2025-01-29 13:21:52 +0100

    andere fourier filter datei

--- SlidingFrankWolfe/__init__.py
+++ SlidingFrankWolfe/__init__.py
@@ -1,5 +1,6 @@
 from .simple_function import *
 from .utils import *
-from .fourier_filter import *
+#from .fourier_filter import *
 from .sliding_optimizer import *
+from .fourier_filter_debugging import *
 
