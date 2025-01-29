5acd27f0c7c36f11855c70d3180e6938d3c7b306
Lena Schmedt <132449066+lenasme@users.noreply.github.com>
2025-01-29 14:00:00 +0100

    Update __init__.py

--- SlidingFrankWolfe/__init__.py
+++ SlidingFrankWolfe/__init__.py
@@ -1,4 +1,5 @@
-from .simple_function import *
+#from .simple_function import *
+from .simple_function_debugging import *
 from .utils import *
 #from .fourier_filter import *
 from .sliding_optimizer import *
