91ffe31d27271d096d29cbbecd41547707974727
lenasme <lena.schmedt@gmx.de>
2025-01-30 10:48:00 +0100

    print hinzu

--- SlidingFrankWolfe/fourier_filter_debugging.py
+++ SlidingFrankWolfe/fourier_filter_debugging.py
@@ -99,7 +99,7 @@ def generate_triangle_aux(grid, cut_off, normalization):
 
                         # Fourier-Transformation für den Punkt (x, y)
                         fft_image = np.exp(-2j * np.pi * (freq_x * x + freq_y * y))
-                        print("freq_x * x + freq_y * y Min/Max:", np.min(freq_x * x + freq_y * y), np.max(freq_x * x + freq_y * y))
+                        #print("freq_x * x + freq_y * y Min/Max:", np.min(freq_x * x + freq_y * y), np.max(freq_x * x + freq_y * y))
 
                         #print("Shape von fft_image:", fft_image.shape)
                         #print("Shape von np.sum(fft_image, axis=0):", np.sum(fft_image, axis=0).shape)
@@ -107,11 +107,11 @@ def generate_triangle_aux(grid, cut_off, normalization):
                         #print("Max:", np.max(np.abs(fft_image)))
                         #print("Mean:", np.mean(np.abs(fft_image)))
                         #print("Std Dev:", np.std(np.abs(fft_image)))
-                        plt.imshow(np.abs(fft_image), origin="lower", cmap="inferno")
-                        plt.colorbar()
-                        plt.title("FFT-Bild vor Maskierung")
-                        plt.show()
-
+                        #plt.imshow(np.abs(fft_image), origin="lower", cmap="inferno")
+                        #plt.colorbar()
+                        #plt.title("FFT-Bild vor Maskierung")
+                        #plt.show()
+                        print("fft_image unique values:", np.unique(np.abs(fft_image)))
                         # Anwenden der Frequenzmaske
                         fft_filtered = fft_image * mask
 
