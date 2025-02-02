dbc01fd4bdf3f8269639341d7ade872557e577b3
lenasme <lena.schmedt@gmx.de>
2025-02-02 17:24:09 +0100

    debugging

--- SlidingFrankWolfe/simple_function_debugging.py
+++ SlidingFrankWolfe/simple_function_debugging.py
@@ -109,8 +109,8 @@ class SimpleFunction:
 
         max_num_triangles = max(len(atom.support.mesh_faces) for atom in self.atoms)
         meshes = np.zeros((self.num_atoms, max_num_triangles, 3, 2))
-        obs = np.zeros((self.num_atoms, max_num_triangles, fourier.grid_size**2))
-       
+        #obs = np.zeros((self.num_atoms, max_num_triangles, fourier.grid_size**2))
+        obs = np.zeros((self.num_atoms,  fourier.grid_size**2))
         for i in range(self.num_atoms):
             support_i = self.atoms[i].support
             meshes[i, :len(support_i.mesh_faces)] = support_i.mesh_vertices[support_i.mesh_faces]
