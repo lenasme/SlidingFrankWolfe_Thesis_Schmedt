c144626c279e75058090fd02651e361dad54db1f
lenasme <lena.schmedt@gmx.de>
2025-02-03 16:48:05 +0100

    debugging

--- SlidingFrankWolfe/fourier_filter_debugging_mesh.py
+++ SlidingFrankWolfe/fourier_filter_debugging_mesh.py
@@ -118,7 +118,10 @@ def generate_triangle_aux(grid, cut_off,  normalization):
                         point = Point(norm_x, norm_y)
 
         
-                        if (rectangle_polygon.contains(point) or rectangle_polygon.boundary.contains(point)) and (triangle_polygon.contains(point) or triangle_polygon.boundary.contains(point)):
+                        if not (triangle_polygon.contains(point) or triangle_polygon.boundary.contains(point)):
+                            function_grid[x, y] = 0
+                            
+                        elif  (triangle_polygon.contains(point) or triangle_polygon.boundary.contains(point) ) and(rectangle_polygon.contains(point) or rectangle_polygon.boundary.contains(point)) :
                             function_grid[x, y] = function.atoms[i].inner_value
                         
 
