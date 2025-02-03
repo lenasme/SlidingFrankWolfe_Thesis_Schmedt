bd6dc5def8946130fbc3ba2eb81c4d759029cdc7
lenasme <lena.schmedt@gmx.de>
2025-02-03 08:55:05 +0100

    debugging

--- SlidingFrankWolfe/fourier_filter_debugging.py
+++ SlidingFrankWolfe/fourier_filter_debugging.py
@@ -140,7 +140,7 @@ def generate_triangle_aux(grid, cut_off,  normalization):
 
             #print("fft_filtered shape:", fft_filtered.shape)
             #res[i,1, :] = fft_filtered.flatten()   
-            res[i, :] = shifted_fft_image.flatten()  
+            res[i, :] = shifted_fft_image.flatten(order="F")  
             plt.plot()
             plt.imshow(np.abs(np.fft.ifft2(res[i, :].reshape((100, 100)))), cmap="bwr")
             plt.colorbar()
