9fe534aae9a3a176c4efb947d333479069af3b1b
lenasme <lena.schmedt@gmx.de>
2025-02-03 15:09:23 +0100

    debugging

--- SlidingFrankWolfe/utils.py
+++ SlidingFrankWolfe/utils.py
@@ -4,7 +4,7 @@ import itertools
 
 from scipy import interpolate
 from shapely.geometry import Polygon
-from .simple_function_debugging import SimpleFunction, ZeroWeightedIndicatorFunction
+from .simple_function_debugging_mesh import SimpleFunction, ZeroWeightedIndicatorFunction
 
 import matplotlib.ticker as tick
 from matplotlib import rc
