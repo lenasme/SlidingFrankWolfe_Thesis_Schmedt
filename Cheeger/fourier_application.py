f6680472a533e65cbdce2ca449fb82556647e5fd
lenasme <lena.schmedt@gmx.de>
2025-02-25 11:25:13 +0100

    integrate on rectangle debuggen

--- Cheeger/fourier_application.py
+++ Cheeger/fourier_application.py
@@ -134,7 +134,7 @@ def plot_triangles_with_image(reconstructed_vanish, triangles):
 
         # Optional: Eckpunkte markieren
         for j in range(3):
-            ax.plot(tri[j, 0], tri[j, 1], 'ro')  # Eckpunkte rot markieren
+            ax.plot(tri[j, 1], tri[j, 0], 'ro')  # Eckpunkte rot markieren
 
     # 3️⃣ Achsen und Titel
     ax.set_title('Reconstructed Image mit Dreiecken')
