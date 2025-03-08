f9bcaf73ca918423209d9756c4d8107f1a3d27cf
lenasme <lena.schmedt@gmx.de>
2025-03-08 17:26:44 +0100

    versuch neue logik

--- Cheeger/compute_cheeger.py
+++ Cheeger/compute_cheeger.py
@@ -4,7 +4,7 @@ import time
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 #from .simple_set import SimpleSet
-from .optimizer_debugging import CheegerOptimizer
+#from .optimizer_debugging import CheegerOptimizer
 from .tools import run_primal_dual, extract_contour, resample
 from .plot_utils import plot_primal_dual_results, plot_simple_set, plot_rectangular_set
 #from .rectangular_optimizer import objective
@@ -46,9 +46,9 @@ def compute_cheeger_set(grid_size, deltas, max_jumps, grid_size_coarse, cut_off,
 
 
 
-def compute_cheeger(eta, grid_size_fm, max_iter_fm=10000, convergence_tol_fm=None, plot_results_fm=False,
-                    num_boundary_vertices_ld=None, point_density_ld=None, max_tri_area_ld=5e-3, step_size_ld=1e-2,
-                    max_iter_ld=500, convergence_tol_ld=1e-4, num_iter_resampling_ld=None, plot_results_ld=False):
+#def compute_cheeger(eta, grid_size_fm, max_iter_fm=10000, convergence_tol_fm=None, plot_results_fm=False,
+                    #num_boundary_vertices_ld=None, point_density_ld=None, max_tri_area_ld=5e-3, step_size_ld=1e-2,
+                   # max_iter_ld=500, convergence_tol_ld=1e-4, num_iter_resampling_ld=None, plot_results_ld=False):
     """
     Compute the Cheeger set associated to the weight function eta
 
@@ -88,21 +88,21 @@ def compute_cheeger(eta, grid_size_fm, max_iter_fm=10000, convergence_tol_fm=Non
         Values of the objective gradient norm over the course of the local descent
 
     """
-    assert (num_boundary_vertices_ld is None) or (point_density_ld is None)
+    #assert (num_boundary_vertices_ld is None) or (point_density_ld is None)
 
     # compute the integral of the weight function on each pixel of the grid
-    eta_bar = eta.integrate_on_pixel_grid(grid_size_fm)
+    #eta_bar = eta.integrate_on_pixel_grid(grid_size_fm)
     
     
 
 
     # perform the fixed mesh optimization step
-    u = run_primal_dual(grid_size_fm, eta_bar, max_iter=max_iter_fm, convergence_tol=convergence_tol_fm, plot=True)
+   # u = run_primal_dual(grid_size_fm, eta_bar, max_iter=max_iter_fm, convergence_tol=convergence_tol_fm, plot=True)
 
-    if plot_results_fm:
-        plot_primal_dual_results(u[1:-1, 1:-1], eta_bar)
+    #if plot_results_fm:
+    #    plot_primal_dual_results(u[1:-1, 1:-1], eta_bar)
 
-    boundary_vertices = extract_contour(u)
+    #boundary_vertices = extract_contour(u)
 
 
     ### Kontrolle, ob erste Kontur passt
@@ -121,19 +121,19 @@ def compute_cheeger(eta, grid_size_fm, max_iter_fm=10000, convergence_tol_fm=Non
 
                       
     # initial set for the local descent
-    boundary_vertices = resample(boundary_vertices, num_boundary_vertices_ld, point_density_ld)
-    simple_set = SimpleSet(boundary_vertices, max_tri_area_ld)
+    #boundary_vertices = resample(boundary_vertices, num_boundary_vertices_ld, point_density_ld)
+   # simple_set = SimpleSet(boundary_vertices, max_tri_area_ld)
 
     ### test
 
-    plot_simple_set(simple_set, eta=eta, display_inner_mesh=False)
+    #plot_simple_set(simple_set, eta=eta, display_inner_mesh=False)
     #print(simple_set.boundary_vertices)
 
-    weighted_area = simple_set.compute_weighted_area(eta)      
-    perimeter = simple_set.compute_perimeter()
-    print("integral fixed mesh:", weighted_area)
-    print("perimeter fixed mesh:", perimeter)
-    print("objective fixed mesh:", perimeter / np.abs(weighted_area))
+   # weighted_area = simple_set.compute_weighted_area(eta)      
+   # perimeter = simple_set.compute_perimeter()
+   # print("integral fixed mesh:", weighted_area)
+   # print("perimeter fixed mesh:", perimeter)
+   # print("objective fixed mesh:", perimeter / np.abs(weighted_area))
     ###
                       
     # perform the local descent step
@@ -157,49 +157,49 @@ def compute_cheeger(eta, grid_size_fm, max_iter_fm=10000, convergence_tol_fm=Non
                       
     #x_values = [v[0] for v in cheeger_set.boundary_vertices]
     #y_values = [v[1] for v in cheeger_set.boundary_vertices]
-    x_values = [v[0] for v in simple_set.boundary_vertices]
-    y_values = [v[1] for v in simple_set.boundary_vertices]                 
+    #x_values = [v[0] for v in simple_set.boundary_vertices]
+   # y_values = [v[1] for v in simple_set.boundary_vertices]                 
 
-    x_min= np.clip(min(x_values), 0,1)
-    x_max= np.clip(max(x_values), 0,1)
-    y_min= np.clip(min(y_values), 0,1)
-    y_max= np.clip(max(y_values), 0,1)
+   # x_min= np.clip(min(x_values), 0,1)
+   # x_max= np.clip(max(x_values), 0,1)
+   # y_min= np.clip(min(y_values), 0,1)
+    #y_max= np.clip(max(y_values), 0,1)
    
-    print("x_min", x_min, "x_max", x_max, "y_min", y_min, "y_max", y_max )
+   # print("x_min", x_min, "x_max", x_max, "y_min", y_min, "y_max", y_max )
 
-    outer_vertices= np.array([x_min, x_max, y_min, y_max])
-    rectangle_boundary_vertices= np.array([[x_min,y_min], [x_min, y_max], [x_max, y_max], [x_max, y_min]])
+    #outer_vertices= np.array([x_min, x_max, y_min, y_max])
+   # rectangle_boundary_vertices= np.array([[x_min,y_min], [x_min, y_max], [x_max, y_max], [x_max, y_min]])
 
     #outer_vertices= np.array([1-x_max, 1-x_min, y_min, y_max])
     #rectangle_boundary_vertices= np.array([[1-x_max,y_min], [1-x_max, y_max], [1-x_min, y_max], [1-x_min, y_min]])
-    rectangle_set = RectangularSet(rectangle_boundary_vertices)
+   # rectangle_set = RectangularSet(rectangle_boundary_vertices)
 
-    print("Starting value objective rectangular:", rectangle_set.compute_perimeter_rec() /  np.abs(rectangle_set.compute_weighted_area_rec(eta)))
-    print("Starting perimeter:", rectangle_set.compute_perimeter_rec())
-    print("Starting area value integral:", np.abs(rectangle_set.compute_weighted_area_rec(eta)))
+   # print("Starting value objective rectangular:", rectangle_set.compute_perimeter_rec() /  np.abs(rectangle_set.compute_weighted_area_rec(eta)))
+   # print("Starting perimeter:", rectangle_set.compute_perimeter_rec())
+   # print("Starting area value integral:", np.abs(rectangle_set.compute_weighted_area_rec(eta)))
 
-    start = time.time()
+   # start = time.time()
 
-    result = minimize(objective, outer_vertices, args=(eta,), bounds=[(0,1),(0,1), (0,1), (0,1)] , options={'maxiter': 10000, 'disp': True, 'ftol': 1e-7, 'gtol': 1e-6})
+  #  result = minimize(objective, outer_vertices, args=(eta,), bounds=[(0,1),(0,1), (0,1), (0,1)] , options={'maxiter': 10000, 'disp': True, 'ftol': 1e-7, 'gtol': 1e-6})
                       
-    end = time.time()
+   # end = time.time()
 
-    optimal_rectangle = result.x
-    optimal_objective = result.fun
-    print("Optimales Rechteck:", optimal_rectangle)
-    print("Optimales Objective:", optimal_objective)
+    #optimal_rectangle = result.x
+    #optimal_objective = result.fun
+    #print("Optimales Rechteck:", optimal_rectangle)
+   # print("Optimales Objective:", optimal_objective)
                       
-    opt_rectangle_boundary_vertices= np.array([[optimal_rectangle[0], optimal_rectangle[2]], [optimal_rectangle[0], optimal_rectangle[3]], [optimal_rectangle[1], optimal_rectangle[3]], [optimal_rectangle[1], optimal_rectangle[2]]])
-    opt_rect_set = RectangularSet(opt_rectangle_boundary_vertices)      
+   # opt_rectangle_boundary_vertices= np.array([[optimal_rectangle[0], optimal_rectangle[2]], [optimal_rectangle[0], optimal_rectangle[3]], [optimal_rectangle[1], optimal_rectangle[3]], [optimal_rectangle[1], optimal_rectangle[2]]])
+   # opt_rect_set = RectangularSet(opt_rectangle_boundary_vertices)      
     
-    plot_rectangular_set(opt_rect_set, eta=eta, display_inner_mesh=False)
-    print("Die Berechnung des Rechtecks hat ", end - start, " Sekunden gedauert." )
-    print("Perimeter:", opt_rect_set.compute_perimeter_rec())
-    print("Value integral :", opt_rect_set.compute_weighted_area_rec(eta))
-    print("Objective:", opt_rect_set.compute_perimeter_rec()/ np.abs(opt_rect_set.compute_weighted_area_rec(eta)))
+   # plot_rectangular_set(opt_rect_set, eta=eta, display_inner_mesh=False)
+  #  print("Die Berechnung des Rechtecks hat ", end - start, " Sekunden gedauert." )
+   # print("Perimeter:", opt_rect_set.compute_perimeter_rec())
+  #  print("Value integral :", opt_rect_set.compute_weighted_area_rec(eta))
+  #  print("Objective:", opt_rect_set.compute_perimeter_rec()/ np.abs(opt_rect_set.compute_weighted_area_rec(eta)))
                       
     #return simple_set, obj_tab, grad_norm_tab, opt_rect_set
-    return simple_set, opt_rect_set
+    #return simple_set, opt_rect_set
 
 
 #def rectangular_cheeger 
