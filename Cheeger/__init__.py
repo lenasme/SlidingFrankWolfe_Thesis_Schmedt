d42af985cd998152a4f38c15abf43063b266948d
lenasme <lena.schmedt@gmx.de>
2025-03-09 12:23:51 +0100

    importe in initialisierung reduziert

--- Cheeger/__init__.py
+++ Cheeger/__init__.py
@@ -1,9 +1,8 @@
-from .compute_cheeger import compute_cheeger
-from .simple_set import *
-from .integrable_function import IntegrableFunction
-from .tools import winding, resample
-from .plot_utils import *
+#from .compute_cheeger import compute_cheeger
+#from .simple_set import *
+#from .integrable_function import IntegrableFunction
+#from .tools import winding, resample
+#from .plot_utils import *
 #from .rectangular_set import *
-from .rectangular_set_debugging import *
-from .fourier_application import *
-from .optimizer_debugging import *
+from .rectangular_set import *
+#from .optimizer_debugging import *
