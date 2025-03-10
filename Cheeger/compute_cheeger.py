c08f3935d665f3073e52e7b85a841a6afed9b2db
lenasme <lena.schmedt@gmx.de>
2025-03-10 18:53:02 +0100

    ground truth von hand

--- Cheeger/compute_cheeger.py
+++ Cheeger/compute_cheeger.py
@@ -22,10 +22,10 @@ def compute_cheeger_set(grid_size, deltas, max_jumps, grid_size_coarse, cut_off,
     #ground_truth = construction_of_example_source(grid_size, deltas, max_jumps)
 
     ground_truth = np.zeros((grid_size, grid_size))
-    x_min, x_max = 0,30
+    x_min, x_max = 0,100
     y_min, y_max = 0,100
 
-    ground_truth[x_min:x_max, y_min:y_max] = 5
+    ground_truth[x_min:x_max, y_min:y_max] = 1
 
     operator_applied_on_ground_truth = np.fft.fft2(ground_truth)
 
