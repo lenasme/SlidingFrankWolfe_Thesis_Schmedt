94dadc9d1f434ec0aa070d90730a600c0a8b36ef
lenasme <lena.schmedt@gmx.de>
2025-03-10 11:58:20 +0100

    plot anpassen

--- Cheeger/rectangular_set.py
+++ Cheeger/rectangular_set.py
@@ -47,8 +47,8 @@ class RectangularSet:
 
 		fig.colorbar(im, ax=ax)
 
-		y_curve = np.append([self.x_min, self.x_max, self.x_max, self.x_min], self.x_min)
-		x_curve =  1-np.append([self.y_min, self.y_min,  self.y_max, self.y_max], self.y_min)
+		y_curve = 1-np.append([self.x_min, self.x_max, self.x_max, self.x_min], self.x_min)
+		x_curve =  np.append([self.y_min, self.y_min,  self.y_max, self.y_max], self.y_min)
 
 		ax.plot(x_curve, y_curve, color='black')
 
