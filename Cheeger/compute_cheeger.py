4be6484034680ce0ff9c30134117e4e416bc3963
lenasme <lena.schmedt@gmx.de>
2025-03-10 12:34:19 +0100

    debugging

--- Cheeger/compute_cheeger.py
+++ Cheeger/compute_cheeger.py
@@ -97,6 +97,9 @@ def compute_cheeger_set(grid_size, deltas, max_jumps, grid_size_coarse, cut_off,
 
     optimal_rectangle, objective_tab, gradient_tab =  run_fine_optimization(initial_rectangular_set, cut_off, weights, grid_size )
 
+    if plot == True:
+        optimal_rectangle.plot_rectangular_set(np.fft.ifft2(truncated_operator_applied_on_ground_truth).real, grid_size)
+
 
 
 
