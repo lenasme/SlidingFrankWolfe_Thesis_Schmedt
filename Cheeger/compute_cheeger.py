5611eba991ddb5d7a831db908a853734bcccbb38
lenasme <lena.schmedt@gmx.de>
2025-03-10 18:31:58 +0100

    ground truth von hand

--- Cheeger/compute_cheeger.py
+++ Cheeger/compute_cheeger.py
@@ -98,7 +98,7 @@ def compute_cheeger_set(grid_size, deltas, max_jumps, grid_size_coarse, cut_off,
 
     x_min, x_max, y_min, y_max = initial_rectangular_set.coordinates[0], initial_rectangular_set.coordinates[1], initial_rectangular_set.coordinates[2], initial_rectangular_set.coordinates[3]
 
-    modified_rectangle = RectangularSet( y_min ,y_max, x_min, x_max)
+    modified_rectangle = RectangularSet( x_min ,x_max, y_min, y_max)
 
     if plot == True:
         modified_rectangle.plot_rectangular_set(np.fft.ifft2(truncated_operator_applied_on_ground_truth).real, grid_size)
