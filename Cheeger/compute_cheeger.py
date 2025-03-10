14832e6a1c41812b83b1b91d6eb2a765453151e0
lenasme <lena.schmedt@gmx.de>
2025-03-10 13:25:42 +0100

    debugging

--- Cheeger/compute_cheeger.py
+++ Cheeger/compute_cheeger.py
@@ -93,7 +93,9 @@ def compute_cheeger_set(grid_size, deltas, max_jumps, grid_size_coarse, cut_off,
     weights = truncated_operator_applied_on_ground_truth
     test_x = np.array(initial_rectangular_set.coordinates, dtype=np.float64, order='F')
     print("objective:",initial_rectangular_set.compute_objective_wrapper(test_x, cut_off, weights, grid_size))
-    print(initial_rectangular_set.objective_gradient_wrapper(test_x, cut_off, weights, grid_size))
+    print("perimeter:", initial_rectangular_set.compute_anisotropic_perimeter())
+    print("integral:", initial_rectangular_set.compute_integral(cut_off, weights, grid_size))
+    print("gradient:", initial_rectangular_set.objective_gradient_wrapper(test_x, cut_off, weights, grid_size))
 
     optimal_rectangle, objective_tab, gradient_tab =  run_fine_optimization(initial_rectangular_set, cut_off, weights, grid_size )
 
