37e25f0574f5734626569848e469bf59a76b5ddf
lenasme <lena.schmedt@gmx.de>
2025-03-10 10:56:47 +0100

    plot_utils hinzu

--- Cheeger/__init__.py
+++ Cheeger/__init__.py
@@ -2,7 +2,7 @@ from .compute_cheeger import *
 #from .simple_set import *
 #from .integrable_function import IntegrableFunction
 #from .tools import winding, resample
-#from .plot_utils import *
+from .plot_utils import *
 #from .rectangular_set import *
 from .rectangular_set import *
 from .optimizer_debugging import *
