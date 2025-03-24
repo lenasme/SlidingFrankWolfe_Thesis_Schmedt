f7341039074fff9bd98edf6d43c5f01472c184e1
lenasme <lena.schmedt@gmx.de>
2025-03-24 09:39:46 +0100

    parallelisieren

--- SlidingFrankWolfe/simple_function.py
+++ SlidingFrankWolfe/simple_function.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from celer import Lasso
+from joblib import Parallel, delayed
 
 from Cheeger.rectangular_set import RectangularSet
 
@@ -205,25 +206,25 @@ class SimpleFunction:
 			return sum_result
 
 
-	def compute_error_term(self, target_function_f):
+	#def compute_error_term(self, target_function_f):
 
-		"""
-		1/2 \sum__{k1, k2} | \sum_j a_j \int_{x_min_j}^{x_max_j} \int_{y_min_j}^{y_max_j} e^{-2 pi i (x*k1 / grid_size + y*k2/ grid_size)} dy dx - f(k1, k2)|^2
-		"""
-		image = np.zeros((self.grid_size, self.grid_size), dtype = complex)
-		res = 0
-		for k1 in range(- self.cut_off, self.cut_off +1):
-			for k2 in range(-self.cut_off, self.cut_off +1):
+	#	"""
+	#	1/2 \sum__{k1, k2} | \sum_j a_j \int_{x_min_j}^{x_max_j} \int_{y_min_j}^{y_max_j} e^{-2 pi i (x*k1 / grid_size + y*k2/ grid_size)} dy dx - f(k1, k2)|^2
+	#	"""
+		#image = np.zeros((self.grid_size, self.grid_size), dtype = complex)
+		#res = 0
+		#for k1 in range(- self.cut_off, self.cut_off +1):
+		#	for k2 in range(-self.cut_off, self.cut_off +1):
 
-				if k1==0 and k2==0:
-					continue
+		#		if k1==0 and k2==0:
+		#			continue
 
-				val = self.compute_fourier_integral(k1, k2)
-				image[(k1+self.grid_size) % self.grid_size, (k2+self.grid_size) % self.grid_size] = val
+		#		val = self.compute_fourier_integral(k1, k2)
+		#		image[(k1+self.grid_size) % self.grid_size, (k2+self.grid_size) % self.grid_size] = val
 
 
-
-				res += np.abs(val - target_function_f[(k1+self.grid_size) % self.grid_size, (k2+self.grid_size) % self.grid_size])**2
+#
+#				res += np.abs(val - target_function_f[(k1+self.grid_size) % self.grid_size, (k2+self.grid_size) % self.grid_size])**2
 
 		#plt.subplot(1,3,1)
 		#plt.tight_layout()
@@ -240,7 +241,29 @@ class SimpleFunction:
 		#plt.colorbar()
 		#plt.show()
 
-		return 0.5 * res
+#		return 0.5 * res
+
+	def compute_error_term(self, target_function_f, n_jobs=-1):
+		"""
+		Parallelisierte Version:
+		1/2 * sum_{k1, k2} |FourierIntegral(k1, k2) - f(k1, k2)|^2
+		"""
+
+		k_vals = np.arange(-self.cut_off, self.cut_off + 1)
+		k1_k2_pairs = [(k1, k2) for k1 in k_vals for k2 in k_vals if not (k1 == 0 and k2 == 0)]
+
+		def compute_single_term(k1, k2):
+			val = self.compute_fourier_integral(k1, k2)
+			target_val = target_function_f[(k1 + self.grid_size) % self.grid_size,
+									   (k2 + self.grid_size) % self.grid_size]
+			return np.abs(val - target_val) ** 2
+
+		# Parallelisiere die Berechnung über alle (k1, k2) Paare
+		results = Parallel(n_jobs=n_jobs, backend='loky')(
+			delayed(compute_single_term)(k1, k2) for k1, k2 in k1_k2_pairs
+		)
+
+		return 0.5 * np.sum(results)
 
 
 	def compute_objective_sliding(self, target_function_f, reg_param):
