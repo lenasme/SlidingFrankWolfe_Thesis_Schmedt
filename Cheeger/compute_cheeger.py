0ab0913acda4d12399655176379802c90b6afe67
lenasme <lena.schmedt@gmx.de>
2025-03-27 15:48:37 +0100

    standard optimization

--- Cheeger/compute_cheeger.py
+++ Cheeger/compute_cheeger.py
@@ -435,6 +435,90 @@ def optimization ( ground_truth, target_function_f, grid_size, grid_size_coarse,
 
 
 
+def standard_optimization( ground_truth, target_function_f, grid_size, grid_size_coarse, cut_off, reg_param, max_iter_primal_dual = 10000, plot=True):
+	
+	atoms = []
+	u = SimpleFunction(atoms, grid_size, cut_off)
+
+	objective_whole_iteration = []
+	objective_overall_development = []
+	iteration = 0
+	max_iter = 30
+
+	convergence = False
+
+	objective_whole_iteration.append(u.compute_objective_sliding( target_function_f, reg_param))
+	objective_overall_development.append(u.compute_objective_sliding( target_function_f, reg_param))
+
+	while not convergence and iteration < max_iter:   
+
+		weights_in_eta = - u.compute_truncated_frequency_image_sf( plot = True) + target_function_f
+
+		optimal_rectangle = compute_cheeger_set(weights_in_eta, grid_size, grid_size_coarse, cut_off, max_iter_primal_dual = 10000, plot=True)
+
+		
+		u.extend_support(optimal_rectangle)
+
+		objective_overall_development.append(u.compute_objective_sliding( target_function_f, reg_param))
+		
+		fit_weights(u,  target_function_f, reg_param)
+
+		objective_overall_development.append(u.compute_objective_sliding( target_function_f, reg_param))
+
+	
+
+		if plot == True:
+
+			fig, ax = plt.subplots(1, 3, figsize=(18, 6))  # 1 Zeile, 2 Spalten
+
+			# Linker Plot mit Funktionsaufruf
+			data = u.construct_image_matrix_sf(plot=False) 
+			vmin = min(np.min(data), np.min(ground_truth))
+			vmax = max(np.max(data), np.max(ground_truth))
+
+			im1 = ax[0].imshow(data, cmap="bwr", vmin=vmin,
+							   vmax=vmax)  
+			fig.colorbar(im1, ax=ax[0])
+			ax[0].set_title("Current Function")
+
+			im2 = ax[1].imshow(ground_truth, cmap = 'bwr', vmin=vmin, vmax=vmax)
+
+			fig.colorbar(im2, ax = ax[1])
+			ax[1].set_title("Ground Truth")
+
+			diff = - data + ground_truth
+			vmax_diff = np.max(np.abs(diff))
+			im3 = ax[2].imshow(diff, cmap = 'bwr', vmin=-vmax_diff, vmax=vmax_diff)
+			fig.colorbar(im3, ax = ax[2])
+			ax[2].set_title("Difference")
+
+			plt.tight_layout()
+
+			plt.show()
+
+		
+		plt.figure()
+		plt.plot(objective_overall_development)
+		plt.title("Objective overall development")
+		plt.show()
+
+
+		plt.figure()
+		plt.plot(objective_whole_iteration)
+		plt.title("Objective development each iteration")
+		plt.show()
+
+		convergence = ((objective_whole_iteration[-2] - objective_whole_iteration[-1] ) < 10 )
+
+		iteration += 1
+
+
+
+
+
+
+
+
 
 def optimization_with_sliding ( ground_truth, target_function_f, grid_size, grid_size_coarse, cut_off, reg_param, max_iter_primal_dual = 10000, plot=True):
 	
