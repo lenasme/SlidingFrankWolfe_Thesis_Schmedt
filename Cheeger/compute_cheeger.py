48016baa09b7bb87cad0676f2f5a2fb1a9207953
lenasme <lena.schmedt@gmx.de>
2025-04-16 11:12:34 +0200

    l1 error

--- Cheeger/compute_cheeger.py
+++ Cheeger/compute_cheeger.py
@@ -725,7 +725,7 @@ def optimization_with_sliding ( ground_truth, target_function_f, grid_size, grid
 		objective_overall_development.append(u.compute_objective_sliding( target_function_f, reg_param))
 		
 		#fit_weights(u, grid_size, cut_off, reg_param, target_function_f)
-		fit_weights(u, target_function_f, reg_param)
+		#fit_weights(u, target_function_f, reg_param)
 
 		if iteration in save_iterations:
 			vmin = min(np.min(ground_truth), -1)  
