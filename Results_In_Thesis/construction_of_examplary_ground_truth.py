7afa2402609c49dc1a1a1a0f1d1032454177a5b0
lenasme <lena.schmedt@gmx.de>
2025-04-18 14:55:04 +0200

    plot anpassen

--- Results_In_Thesis/construction_of_examplary_ground_truth.py
+++ Results_In_Thesis/construction_of_examplary_ground_truth.py
@@ -8,7 +8,7 @@ from Setup.ground_truth import GroundTruth
 
 
 
-def construct_jump_points(seed, grid_size, deltas, max_jumps, cut_off, u_filepaths, labels, colors=None):
+def construct_jump_points(seed, grid_size, deltas, max_jumps, cut_off, u_filepaths, labels, loc, colors=None):
 	
 	original = GroundTruth(grid_size, max_jumps, seed)
 
@@ -52,7 +52,7 @@ def construct_jump_points(seed, grid_size, deltas, max_jumps, cut_off, u_filepat
 		if proxy:
 			proxy.set_label(label)
 	
-	ax.legend(loc='lower left', fontsize='medium', frameon=True)
+	ax.legend(loc=loc, fontsize='medium', frameon=True)
 	plt.tight_layout()
 	plt.savefig(fr"C:\Lena\Universität\Inhaltlich\Master\AMasterarbeit\Masterarbeit_Dokument\ground_truth_jump_points_gradient_support_seed{seed}_cutoff{cut_off}.png", dpi=300)
 	plt.show()
