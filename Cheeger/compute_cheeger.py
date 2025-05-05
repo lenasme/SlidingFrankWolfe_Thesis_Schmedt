d6d7006495f0ca7f346793b593337fee8f1c045d
lenasme <lena.schmedt@gmx.de>
2025-05-05 10:51:29 +0200

    debugging

--- Cheeger/compute_cheeger.py
+++ Cheeger/compute_cheeger.py
@@ -343,7 +343,7 @@ def standard_optimization( ground_truth, target_function_f, grid_size, grid_size
 	#	f.write(latex_command_name)
 
 	#shutil.copy("number_of_atoms_commands.tex", r"C:\Lena\Universität\Inhaltlich\Master\AMasterarbeit\Masterarbeit_Dokument")
-	command_name = f"numberofatomsStandardCutoff{cut_off_str}Seed{seed_str}"
+	command_name = f"numberofatomsStandardCutoff_{cut_off_str}_Seed_{seed_str}"
 	tex_path = "number_of_atoms_commands.tex"
 
 	write_latex_command(command_name, number_of_atoms, tex_path)
@@ -542,14 +542,22 @@ def optimization_with_sliding ( ground_truth, target_function_f, grid_size, grid
 
 	cut_off_str = str(cut_off)
 	seed_str = str(seed)
-	latex_command_name = f"\\newcommand{{\\numberofatomsSlidingCutoff{cut_off_str}Seed{seed_str}}}{{{number_of_atoms}}}\n"
-	with open("number_of_atoms_commands.tex", "a") as f:
-		f.write(latex_command_name)
+	#latex_command_name = f"\\newcommand{{\\numberofatomsSlidingCutoff{cut_off_str}Seed{seed_str}}}{{{number_of_atoms}}}\n"
+	#with open("number_of_atoms_commands.tex", "a") as f:
+	#	f.write(latex_command_name)
 
-	number_of_atoms = u.num_atoms
-	np.save(f"fw_number_of_rectangels_iteration20_cutoff{cut_off}_seed{seed}.npy", number_of_atoms)
+	#number_of_atoms = u.num_atoms
+	#np.save(f"fw_number_of_rectangels_iteration20_cutoff{cut_off}_seed{seed}.npy", number_of_atoms)
 
-	shutil.copy("number_of_atoms_commands.tex", r"C:\Lena\Universität\Inhaltlich\Master\AMasterarbeit\Masterarbeit_Dokument")
+	#shutil.copy("number_of_atoms_commands.tex", r"C:\Lena\Universität\Inhaltlich\Master\AMasterarbeit\Masterarbeit_Dokument")
+
+
+	command_name = f"numberofatomsSlidingCutoff_{cut_off_str}_Seed_{seed_str}"
+	tex_path = "number_of_atoms_commands.tex"
+
+	write_latex_command(command_name, number_of_atoms, tex_path)
+
+	shutil.copy(tex_path, r"C:\Lena\Universität\Inhaltlich\Master\AMasterarbeit\Masterarbeit_Dokument")
 
 
 
